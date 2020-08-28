0c34038cd2af8b1911eb785184cc385683c7d4c8
Vineeth Rajendran <vineeth18@users.noreply.github.com>
2020-08-28 21:02:36 +0530

    Updates

---
+++ precache-manifest.40dae68708392b1706d84f26d5f8dbf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8acab4c16a827bd70b172e38857a2d3c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9dcced687851097c12eb",
+    "url": "/portfolio/static/css/main.f2a40251.chunk.css"
+  },
+  {
+    "revision": "b3d672e5acbd7c7a55d1",
+    "url": "/portfolio/static/js/2.5c1ad7d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.5c1ad7d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dcced687851097c12eb",
+    "url": "/portfolio/static/js/main.63c1804b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
