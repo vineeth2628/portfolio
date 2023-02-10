9d75aede46cc8942215be5f0f3a5b727292ea2a1
Vineeth Rajendran <vineethrajendranofficial@gmail.com>
2023-02-10 15:30:50 +0530

    Updates

---
+++ precache-manifest.d1d5bb6238c92cd8bd39e92aeb815c68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b02647b3295a58f694aca03d356ed3af",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2f0f891980e7d8543e46",
+    "url": "/portfolio/static/css/main.1cb52e43.chunk.css"
+  },
+  {
+    "revision": "a172829f5fa19557d7cf",
+    "url": "/portfolio/static/js/2.739eef42.chunk.js"
+  },
+  {
+    "revision": "191558eb5c2977f0c60c6841502a3f65",
+    "url": "/portfolio/static/js/2.739eef42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f0f891980e7d8543e46",
+    "url": "/portfolio/static/js/main.a733a713.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/portfolio/static/js/main.a733a713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  }
+]);
